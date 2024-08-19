b1dbaff0257e2cc20407b8aaedacc0332807e3b0
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2024-08-19 15:02:59 +0800

    setup tailwind

---
+++ src/layouts/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer> 
+      <p>Made with love and coffee by <a href="https://github.com/salassep" target="_blank">salassep</a></p>
+    </footer>
+  );
+}
