71ded7cfcf129d429429f7396eaf50a20d0f06e0
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2024-08-23 10:36:35 +0800

    update emotion componenet width

--- src/components/Emotion.tsx
+++ src/components/Emotion.tsx
@@ -1,6 +1,9 @@
 export default function Emotion({ name, handleClick }: { name: string, handleClick: () => void }) {
   return (
-    <button onClick={handleClick} className="text-xl bg-primary w-max p-4 rounded-xl cursor-pointer hover:bg-primary-dark">
+    <button 
+      onClick={handleClick} 
+      className="text-xl bg-primary w-max p-4 rounded-xl cursor-pointer hover:bg-primary-dark min-w-[178px]"
+    >
       {name}
     </button>
   )
