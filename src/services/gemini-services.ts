6713168e657e244f5e86719d8b228703c81308aa
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2024-11-25 22:26:37 +0800

    fix: delete generative configuration in gemini services

--- src/services/gemini-services.ts
+++ src/services/gemini-services.ts
@@ -6,31 +6,18 @@ const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
-  tools: [{codeExecution: {}}]
 });
 
-const generationConfig = {
-  temperature: 0.5,
-  topP: 0.95,
-  topK: 64,
-  maxOutputTokens: 8192,
-  responseMimeType: "text/plain" 
-};
-
 export async function getMotivation(emotion: string) {
-  const chatSession = model.startChat({
-    generationConfig,
-  });
-
-  const result = await chatSession.sendMessage(`Give me a qoute, I am feeling ${emotion} right now. Just give the quote not the explanation.`);
+  const result = await model.generateContent(
+    `Give me a qoute, I am feeling ${emotion} right now. Just give the quote not the explanation.`
+  );
   return result.response.text();
 }
 
 export async function changeMotivation(currentMotivation: string) {
-  const chatSession = model.startChat({
-    generationConfig,
-  });
-
-  const result = await chatSession.sendMessage(`${currentMotivation}, but I want something different. Give me a quote with same context. Just give the quote not the explanation.`);
+  const result = await model.generateContent(
+    `I have quote: ${currentMotivation}. Give me a quote with same context. Just give the quote not the explanation.`
+  );
   return result.response.text();
 }
